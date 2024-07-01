d73602a7a0f5db0c26553662dd1a2bcd42acaa0f
Marco Madera <me@marcomadera.com>
2024-07-01 04:11:11 -0700

    add database structure (#13)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,29 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"snippets.marcomadera.com/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	staticDir := flag.String("staticDir", "./ui/static/", "HTTP Static Director to serve")
+	dsn := flag.String("dsn", "web:pass@/snippets?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// Custom logs prefixed, followed by a tab
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -33,6 +48,19 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on: %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
