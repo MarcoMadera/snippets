9f9f07cf61e2fc5bead1d52e306ea52f3683a997
MarcoMadera <me@marcomadera.com>
2024-06-30 23:27:51 -0700

    improvie loggings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,18 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	staticDir := flag.String("staticDir", "./ui/static/", "HTTP Static Director to serve")
 	flag.Parse()
+
+	// Custom logs prefixed, followed by a tab
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir(*staticDir))
@@ -19,7 +25,13 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on: %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on: %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
